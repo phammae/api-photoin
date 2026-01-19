d496f37b404f96cf2e1b475c06580e8d67ad717a
phammae <mamrizal823@gmail.com>
2026-01-19 08:41:56 +0700

    Initial Commit

---
+++ app/Http/Middleware/EnableCors.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middlewares;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnableCors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return \Illuminate\Http\Response|RedirectResponse|JsonResponse
+     */
+    public function handle(Request $request, Closure $next): Response|RedirectResponse|JsonResponse
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+    }
+}
